c4c4ec43b1e01184197e223f72fc77c93c3572c4
BlossomWhale <lkorea12345@gmail.com>
2022-02-13 02:16:06 +0900

    o.o

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -34,9 +35,15 @@ public class ConfigurationSingletonTest {
         System.out.println("memberRepository = " + memberRepository);
 
         //then
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository);
+        Assertions.assertThat(memberRepository2).isSameAs(memberRepository);
+    }
 
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
 
-
-
+        System.out.println("bean = " + bean);
     }
 }
